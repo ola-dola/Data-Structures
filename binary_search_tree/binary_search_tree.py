5cf75f884eb01e355746c3c075fb88e8af36b57b
ola-dola <oredolaolamide@gmail.com>
2020-06-10 20:52:09 +0100

    BST insert method implementation

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,7 +17,16 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if value >= self.value:
+            if not self.right:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+        else:
+            if not self.left:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
