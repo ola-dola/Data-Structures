692204a121be0f910460e3128aed9397969364f9
ola-dola <oredolaolamide@gmail.com>
2020-06-10 15:09:34 +0100

    remove from dll tail method

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -104,7 +104,24 @@ class DoublyLinkedList:
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
     def remove_from_tail(self):
-        pass
+        old_tail = self.tail
+
+        if self.length == 0:
+            return None
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+            self.length = 0
+            return old_tail.value
+
+        new_tail = self.tail.prev
+        new_tail.next = None
+        self.tail = new_tail
+        self.length -= 1
+
+        return old_tail.value
+
+
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
